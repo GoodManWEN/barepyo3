b3e61c5bfa15f5242dd6a81dea336c016975a72c
GoodManWEN <44322449+GoodManWEN@users.noreply.github.com>
2021-03-21 17:55:23 +0000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,30 @@
+#[macro_use] mod macros;
+
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 // use hashbrown::HashMap;
 
+pub trait AnyExt {
+    fn type_name(&self) -> &'static str;
+}
+
+impl<T> AnyExt for T {
+    fn type_name(&self) -> &'static str {
+        std::any::type_name::<T>()
+    }
+}
+
+fn test_with_nogil(
+) -> () {
+}
+
 #[pyfunction]
 fn test(
-
-) -> usize {
-    0
+    py: Python,
+) -> () {
+    py.allow_threads(|| test_with_nogil())
 }
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn testlib(_py: Python, m: &PyModule) -> PyResult<()> {
